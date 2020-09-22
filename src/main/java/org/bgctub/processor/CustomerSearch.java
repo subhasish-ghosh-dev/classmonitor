ffcf0db67cbadd638695a20b8cfb06c66d0ba50b
subhasish-ghosh-dev <moni.ghos@gmail.com>
2020-09-22 05:34:39 +0000

    Binary Search Done

--- src/main/java/org/bgctub/processor/CustomerSearch.java
+++ src/main/java/org/bgctub/processor/CustomerSearch.java
@@ -17,8 +17,16 @@ public class CustomerSearch {
         return this.customers;
     }
 
-    public Customer searchCustomerById(String code){
-        
+    public Customer searchCustomerById(Customer customerObj){
+       
+        Comparator<Customer> c = new Comparator<Customer>() {
+            public int compare(Customer c1, Customer c2){
+               return c1.getCode().compareTo(c2.getCode());
+            }
+        };
+
+        final int idx = Collections.binarySearch(this.customers, customerObj, c);
+        return this.customers.get(idx);
     }
 
 } 
